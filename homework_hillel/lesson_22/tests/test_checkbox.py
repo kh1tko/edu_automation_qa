f80cc34b8c7dd82b84dcd10626384252ad929689
Artem <hitko21@gmail.com>
2024-01-31 20:14:29 +0200

    lesson 22

---
+++ homework_hillel/lesson_22/tests/test_checkbox.py
@@ -0,0 +1,16 @@
+import pytest
+from homework_hillel.lesson_22.CheckboxPage import CheckboxPage
+
+
+@pytest.mark.usefixtures("chrome_class")
+class TestCheckboxPage:
+    def setup(self):
+        self.page = CheckboxPage(self.driver)
+
+    def test_page(self):
+        self.page.open()
+        self.page.expand_folder("home")
+        self.page.mark_folder("home")
+        self.page.collapse_folder("home")
+        self.page.unmark_folder("home")
+        pass
