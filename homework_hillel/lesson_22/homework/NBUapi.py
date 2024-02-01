e8fb6bac0630f1cf3e591d165204c9918401b1df
Artem <hitko21@gmail.com>
2024-02-01 13:31:38 +0200

    HW API testing (small fix)

--- homework_hillel/lesson_22/homework/NBUapi.py
+++ homework_hillel/lesson_22/homework/NBUapi.py
@@ -2,24 +2,26 @@ import requests
 
 
 class CurrencyDataHandler:
-    def __init__(self):
+    def __init__(self, url='https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'):
+        self.url = url
         self.json_data = self.get_currency_data()
 
     def get_currency_data(self):
-        URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
-        response = requests.get(URL)
+        response = requests.get(self.url)
         json_data = response.json()
         return json_data
 
     def write_currency_data_to_file(self):
         creation_date = self.json_data[0].get('exchangedate', '')
-        with open('currency.txt', 'w', encoding='utf-8') as file:
-            file.write(f'[Дата створення запиту]: {creation_date}\n')
 
-            for index, currency_info in enumerate(self.json_data):
-                currency_name = currency_info.get('txt', '')
-                exchange_rate = currency_info.get('rate', '')
-                file.write(f'{index + 1}. {currency_name} to UAH: {exchange_rate}\n')
+        text_to_write = f'[Дата створення запиту]: {creation_date}\n'
+
+        for index, currency_info in enumerate(self.json_data):
+            currency_name = currency_info.get('txt', '')
+            exchange_rate = currency_info.get('rate', '')
+            text_to_write += f'{index + 1}. {currency_name} to UAH: {exchange_rate}\n'
+        with open('currency.txt', 'w', encoding='utf-8') as file:
+            file.write(text_to_write)
 
 
 if __name__ == '__main__':
