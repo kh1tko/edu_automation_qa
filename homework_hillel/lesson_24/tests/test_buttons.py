9da03dcd88742e286d00822d6e0079276326f40c
Artem <hitko21@gmail.com>
2024-02-07 12:43:44 +0200

    hw24

---
+++ homework_hillel/lesson_24/tests/test_buttons.py
@@ -0,0 +1,27 @@
+import pytest
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from homework_hillel.lesson_24.src.pages.page_buttons import PageButtons
+
+
+@pytest.mark.usefixtures("chrome_class")
+class TestButtons:
+
+    def setup_method(self):
+        self.driver: WebDriver = self.driver
+        self.page = PageButtons(driver=self.driver)
+
+    def test_doubleclick_button(self):
+        self.page.open()
+        self.page.button_doubleclick().doubleclick()
+        assert self.page.get_button_doubleclick_message() == 'You have done a double click'
+
+    def test_right_click_button(self):
+        self.page.open()
+        self.page.right_click_button().right_click()
+        assert self.page.get_button_right_click_message() == 'You have done a right click'
+
+    def test_dynamic_id_click_button(self):  # кнопка Click Me
+        self.page.open()
+        self.page.dymanic_id_click_button().click()
+        assert self.page.get_button_dynamic_id_click_message() == 'You have done a dynamic click'
