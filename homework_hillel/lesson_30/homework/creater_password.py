d008eb6f45202b3f57c153fe5cf997057db5ac00
Artem <hitko21@gmail.com>
2024-03-04 12:10:15 +0200

    homework random-password(v.2)

--- homework_hillel/lesson_30/homework/creater_password.py
+++ homework_hillel/lesson_30/homework/creater_password.py
@@ -2,25 +2,32 @@ import random
 import string
 
 
-def generate_password(length, alphabets):
+def generate_password(length: int, alphabets: str) -> list:
     password = ''
     choices = ''
 
-    if 'latin' in alphabets:
-        choices += string.ascii_letters
-    if 'cyrillic' in alphabets:
-        choices += 'абвгдеєжзиіїйклмнопрстуфхцчшщьюяАБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ'
-    if 'numbers' in alphabets:
-        choices += string.digits
+    for alphabet in alphabets:
+        if alphabet.lower() == 'latin':
+            choices += string.ascii_letters
+        elif alphabet.lower() == 'cyrillic':
+            choices += 'абвгдеєжзиіїйклмнопрстуфхцчшщьюяАБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ'
+        elif alphabet.lower() == 'numbers':
+            choices += string.digits
+
+    if 'lower' in alphabets:
+        choices = choices.lower()
+    elif 'upper' in alphabets:
+        choices = choices.upper()
 
     for _ in range(length):
         password += random.choice(choices)
-
     return password
 
 
 if __name__ == '__main__':
-    alphabets = input("Choose alphabets for the password (latin, cyrillic, numbers), separated by commas: ").split(', ')
+    alphabets = input(
+        "Choose alphabets and case selection for the password (latin, cyrillic, numbers, (lower or upper)), separated by commas: ").split(
+        ', ')
     length = int(input("Enter password length: "))
 
     password = generate_password(length, alphabets)
