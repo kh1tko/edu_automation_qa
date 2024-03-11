0aced3e600ffd8a4890937427e178174db2b1ba5
Artem <hitko21@gmail.com>
2024-03-11 11:39:41 +0200

    sqlite3. test version (translated comments)

--- homework_hillel/lesson_31/homework/test_min_price_car.py
+++ homework_hillel/lesson_31/homework/test_min_price_car.py
@@ -1,13 +1,13 @@
 def test_check_car_price_above(db_connection):
-    # Задаємо мінімальну ціну для перевірки
+    # Set a minimal price to check
     min_price = 25000
 
-    # Дістаємо дані із таблиці "cars"
+    # Get up data from table 'cars'
     cursor = db_connection.cursor()
     cursor.execute('SELECT * FROM cars')
 
-    # Перевіряємо чи є хоча би один автомобіль вище ніж наша заданна мінімальна ціна
+    # Test for include any cars with a minimum price from the table
     has_car_above_price = any(row[4] > min_price for row in cursor.fetchall())
 
-    # Асертемо )))
+    # Assert it)))
     assert has_car_above_price, f'No find any auto with price high  {min_price}.'
